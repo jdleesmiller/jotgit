1ae17acb91b5e198059f54fd90e7cc98fec32c45
John Lees-Miller <jdleesmiller@gmail.com>
2014-05-19 21:31:37 +0100

    remove nodegit -- unused for now

--- packages/nodegit/nodegit.js
+++
@@ -1,1 +0,0 @@
-NodeGit = Npm.require("nodegit");
--- packages/nodegit/package.js
+++
@@ -1,8 +0,0 @@
-Package.describe({ summary: "NodeGit" });
-
-Npm.depends({ "nodegit": "0.1.3" });
-
-Package.on_use(function (api) {
-  api.export('NodeGit');
-  api.add_files('nodegit.js', 'server');
-});
